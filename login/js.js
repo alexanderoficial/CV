f8c429de5afff3086a106e11c72f2a6330f126d4
alexanderoficial <138260223+alexanderoficial@users.noreply.github.com>
2023-08-02 14:08:35 -0500

    Update js.js
    login. js

--- login/js.js
+++ login/js.js
@@ -1,15 +1,19 @@
-document.getElementById("login-button").addEventListener("click", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe
-  
-    // Obtén los valores del nombre de usuario y la contraseña
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Validar los datos del inicio de sesión
-    if (username === "user" && password === "123456789") {
-      alert("Inicio de sesión exitoso");
+// Obtener los elementos del formulario y del contenedor de certificaciones
+const loginForm = document.getElementById("loginForm");
+const apodo = document.querySelector(".apodo");
+
+// Agregar un evento de escucha al formulario de inicio de sesión
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Verificar si la contraseña es correcta y mostrar una alerta correspondiente
+    if (password === "Alexander2023") {
+        alert("¡Inicio de sesión exitoso! Bienvenido");
+        // Redireccionar a la página de certificaciones.html
+        window.location.href = 'certificaciones.html';
     } else {
-      alert("Nombre de usuario o contraseña incorrectos");
+        alert("Oops... Contraseña incorrecta");
     }
-  });
-  
+});
